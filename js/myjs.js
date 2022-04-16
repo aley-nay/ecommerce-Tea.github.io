8669a01f7018a0102530d08ae831f58966ce1e08
aleynaben <38936568+aleynaben@users.noreply.github.com>
2022-04-16 13:38:47 +0300

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoPlay: 1000,
+         items:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
